86fe1814c43b0e6f1bec47a2a395c44e2c9182b7
rinsuki <428rinsuki+git@gmail.com>
2021-08-25 16:00:33 +0900

    feat: add umd build for browser and other not-node environments

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import pluginCommonJS from "@rollup/plugin-commonjs"
+import pluginNodeResolve from "@rollup/plugin-node-resolve"
+import { terser } from "rollup-plugin-terser"
+
+export default [{
+    input: "./dist/index.js",
+    output: [{
+        name: "shibunyan",
+        file: __dirname+"/dist/shibunyan.umd.min.js",
+        format: "umd",
+    }],
+    plugins: [
+        pluginCommonJS(),
+        pluginNodeResolve(),
+        terser({
+            mangle: false,
+            output: {
+                comments(node, comment) {
+                    return /license|copyright/i.test(comment.value)
+                },
+                keep_numbers: true,
+            },
+            ecma: 2015,
+        }),
+    ]
+}]
